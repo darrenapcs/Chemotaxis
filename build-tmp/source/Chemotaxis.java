3a223606529045e78f768cf766290a13ed10bd2c
darrenapcs <lgytuan@gmail.com>
2015-09-23 10:47:27 -0700

    nofill

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -15,17 +15,16 @@ import java.io.IOException;
 public class Chemotaxis extends PApplet {
 
 Bacteria [] colony;
-int cirX = mouseX;
-int cirY = mouseY;
+
 
  public void setup()   
  {     
 
  	size(500,500);
- 	colony = new Bacteria[50];
+ 	colony = new Bacteria[100];
  	for(int i = 0; i < colony.length; i++)
  	{
- 		colony [i] = new Bacteria(250,250);
+ 		colony [i] = new Bacteria((int)(Math.random()*500),(int)(Math.random()*500));
  	}	
 
  }
@@ -33,14 +32,13 @@ int cirY = mouseY;
  {    
 
  	background(0);
- 
+
  	for(int i = 0; i < colony.length; i++)
  	{
  	colony[i].move();
  	colony[i].show();	
     }
-   fill(0,150,150); 
-   ellipse(cirX, cirY, 20, 20);
+  
    
  }  
  
@@ -51,7 +49,8 @@ int cirY = mouseY;
  	int myY;
  	int myColor;
  	int mySize;
- 	
+ 	int cirX;
+ 	int cirY;
 
  	Bacteria(int x, int y)
  	{
@@ -59,13 +58,15 @@ int cirY = mouseY;
  		myY = y;
  		myColor = color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
         mySize = 10;
+        cirX = -10;
+        cirY = -10;
     }
      public void move()
 
     {
 
-    myX = myX + (int)(Math.random()*9)-4;
-    myY = myY + (int)(Math.random()*9)-4;
+    myX = myX + (int)(Math.random()*7)-3;
+    myY = myY + (int)(Math.random()*7)-3;
 
     }
 
@@ -73,8 +74,9 @@ int cirY = mouseY;
 
 public void show()
 {
-
-fill(myColor);
+noFill();
+//fill(myColor);
+stroke(myColor);
 ellipse(myX, myY, mySize, mySize);
 if(myX > 500)
 {
@@ -84,7 +86,7 @@ if(myX < 0)
 {
 	myX = 250;
 }
-if(myY > 400)   
+if(myY > 500)   
 {
    myY = 250;
 }
@@ -92,13 +94,24 @@ if(myY < 0)
 { 
    myY = 250;
 }
-if(mousePressed)
+if(mousePressed && mouseButton == LEFT)
 {
 	mySize += 1;
 }
-if(mySize > 60)
+if(mousePressed && mouseButton == RIGHT)
+{
+  cirX = mouseX;
+  cirY = mouseY;
+}
+ fill(0,150,150); 
+ ellipse(cirX, cirY, 20, 20);
+if(mySize > 50)
 {
 	mySize = 5;
+}
+if(get(myX,myY) != color(0))
+{
+	mySize +=1;
 }
 }
 
